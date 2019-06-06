bcf5540985f26fe7e41379034e946e68568b0d0c
jgraffeo <jgraffeo@gmail.com>
2019-06-06 14:39:43 -0700

    final

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,42 +4,42 @@
 var friends = [
     {
     name: "Robert Robertson",
-    photo: "image.png",
+    photo: "https://i.ytimg.com/vi/tVb0g0-JCfk/hqdefault.jpg",
     scores: [2, 3, 5, 2, 1, 4, 2, 5, 3, 2]
     },
     {
     name: "John Johnson",
-    photo: "image.png",
+    photo: "https://chscourier.com/wp-content/uploads/2017/01/80c5ee8341ed0f24ad0c5a3d2b9eac2e.jpg",
     scores: [4, 3, 1, 5, 2, 4, 3, 1, 1, 2]
     },
     {
     name: "Will Wilson",
-    photo: "image.png",
+    photo: "https://comps.canstockphoto.com/old-man-with-a-funny-face-stock-photo_csp5579974.jpg",
     scores: [1, 3, 2, 4, 3, 5, 3, 4, 2, 1]
     },
     {
     name: "Smith Smithers",
-    photo: "image.png",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/8/86/Waylon_Smithers_1.png/220px-Waylon_Smithers_1.png",
     scores: [2, 4, 5, 3, 2, 1, 2, 5, 2, 4]
     },
     {
     name: "George Georgtown",
-    photo: "image.png",
+    photo: "https://latenitedelight.files.wordpress.com/2015/08/bush_confused.jpg",
     scores: [5, 4, 3, 2, 1, 2, 3, 4, 1, 2]
     },
     {
     name: "Sally Salsbury",
-    photo: "image.png",
+    photo: "http://www.peteykins.com/sparklepics3/Condi011707a.jpg",
     scores: [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
     },
     {
     name: "Becky Beckoning",
-    photo: "image.png",
+    photo: "http://thefinereport.com/wp-content/uploads/2013/04/Nancy-Pelosi-squeezing.jpg",
     scores: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
     },
     {
     name: "Ethel Ethelstein",
-    photo: "image.png",
+    photo: "http://worldwideinterweb.com/wp-content/uploads/2016/03/bad-hillary-photos.jpg",
     scores: [3, 3, 4, 3, 5, 5, 2, 2, 3, 4]
     },
 ]; 
